65e4c118cf64895f451465e58860b131fe41a215
Vershinin-V-V <78908865+Vershinin-V-V@users.noreply.github.com>
2021-03-10 16:06:17 +0400

    Create ContactListFragment and ContactDetailsFragment (#2)
    * Create ContactListFragment and ContactDetailsFragment
    
    * Create ContactListFragment and ContactDetailsFragment

---
+++ app/src/main/java/com/classic117/classicproject/ContactDetailsFragment.kt
@@ -0,0 +1,37 @@
+package com.classic117.classicproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+private const val ARG_PERSON_ID = "id"
+
+class ContactDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater : LayoutInflater,
+        container : ViewGroup?,
+        savedInstanceState : Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.contact_details_fragment, container, false)
+    }
+
+    override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)?.supportActionBar?.apply {
+            setTitle(R.string.contact_details_bar)
+        }
+    }
+
+    companion object {
+        fun newInstance(personId : Int) = ContactDetailsFragment().apply {
+            arguments = Bundle().apply {
+                putSerializable(ARG_PERSON_ID, 1) //TODO заменить на id будущего класса Person
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/classic117/classicproject/MainActivity.kt
+++ app/src/main/java/com/classic117/classicproject/MainActivity.kt
@@ -3,9 +3,25 @@ package com.classic117.classicproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PersonSelect{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, ContactListFragment())
+                    .commit()
+        }
     }
+
+    override fun onPersonSelected(id : Int) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, ContactDetailsFragment.newInstance(id))
+            .addToBackStack(null)
+            .commit()
+    }
+
+
 }
